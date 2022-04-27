3eac92d4371a96a2b7aaacab0e8f08bad708f793
Henczes Tas <henczes.gabor.tas@gmail.com>
2022-04-27 22:40:35 +0200

    Login elhelyezés

--- src/App.js
+++ src/App.js
@@ -14,24 +14,11 @@ function App() {
   return (
       <table className='LoginTable'>
         <tr>
-          <td className='login_intro'>
-          <div>
-            <h1 className ='login_title'>Welcome Student!</h1>
-            <br></br>
-            <p className='login_intro_text'> This is the university's platform for you to register sports events and check your status regarding them!Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample Sample </p>
-          </div>
-          <div>
-            <img src={sportOne} width = "60%" className = 'sport_one_img'></img>
-          </div>
-          </td>
           <td>
-            <div>
-              <img src={sportTwo} width = "15%" className = 'sport_two_img'></img>
+            <div className ='login_widget'>
+              {renderState[1] === 'LOGIN' && <LoginBody />}
+              {renderState[1] === 'REGISTER' && <Register />}
             </div>
-          <div className ='login_widget'>
-          {renderState[1] === 'LOGIN' && <LoginBody />}
-          {renderState[1] === 'REGISTER' && <Register />}
-        </div>
           </td>
         </tr>
       </table>
