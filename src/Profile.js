f1e5a703670954b560417c662a26483f8dd89e18
hckrml <84583535+hckrml@users.noreply.github.com>
2022-04-29 22:12:36 +0200

    profile tables aligned for mobile

--- src/Profile.js
+++ src/Profile.js
@@ -1,59 +1,44 @@
-import './App.css';
+import "./App.css";
 
+function Profile() {
+  return (
+    <div>
+      <table className="profileComponentTable">
+        <td className="userProfileData">
+          <div className="profileBody">
+            <h2 className="profile-title">Profile</h2>
+            <table className="profileTable">
+              <tr>
+                <td>Name:</td>
+                <td className="dataColumn">Sample</td>
+              </tr>
+              <tr>
+                <td>Email address:</td>
+                <td className="dataColumn">Sample</td>
+              </tr>
+              <tr>
+                <td>Birth date:</td>
+                <td className="dataColumn">Sample</td>
+              </tr>
+            </table>
+          </div>
+        </td>
 
-function Profile()
-{
-    return(
-        <div>
-           <table className='profileComponentTable'>
-               <tr>
-               <td className='userProfileData'>
-               <div className="profileBody">  
-             <h2 className="profile-title">Profile</h2>
-             <table className='profileTable'>
-                 <tr>
-                     <td>
-                         Name:
-                     </td>
-                     <td className='dataColumn'>
-                         Sample
-                     </td>
-                 </tr>
-                 <tr>
-                     <td>
-                         Email address:
-                     </td>
-                     <td className='dataColumn'>
-                         Sample
-                     </td>
-                </tr>
-                 <tr>
-                     <td>
-                         Birth date:
-                     </td>
-                     <td className='dataColumn'>
-                         Sample
-                     </td>
-                 </tr>
-            </table>            
-           </div>
-               </td>
-                <td>
-                <div className="achivementsBody">  
-             <h2 className="achivements-title">Achivements</h2>
-             <table className='achivementsTable'>
-            </table>            
-           </div>
-           <div className="achivementsBody">  
-             <h2 className="achivements-title">Events registered</h2>
-             <table className='achivementsTable'>
-            </table>            
-           </div>
-                </td>
-               </tr>
-           </table>
-        </div>
-    )
+        <tr>
+          <div className="achivementsBody">
+            <h2 className="achivements-title">Achivements</h2>
+            <table className="achivementsTable"></table>
+          </div>
+        </tr>
+        <tr>
+          <div className="achivementsBody">
+            <h2 className="achivements-title">Events registered</h2>
+            <table className="achivementsTable"></table>
+          </div>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default Profile;
